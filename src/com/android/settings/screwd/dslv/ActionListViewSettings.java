c0e58eb8397bfe474041290ee4af59d98a8f7764
MrApocalypse <ensabahnur16@gmail.com>
2014-12-09 21:54:11 -0600

    Fix merge derpage

--- src/com/android/settings/screwd/dslv/ActionListViewSettings.java
+++ src/com/android/settings/screwd/dslv/ActionListViewSettings.java
@@ -54,12 +54,12 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.internal.util.screwd.ActionConfig;
-import com.android.internal.util.screwd.ActionConstants;
-import com.android.internal.util.screwd.ActionHelper;
-import com.android.internal.util.screwd.ImageHelper;
-import com.android.internal.util.screwd.DeviceUtils;
-import com.android.internal.util.screwd.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.slim.ActionConfig;
+import com.android.internal.util.slim.ActionConstants;
+import com.android.internal.util.slim.ActionHelper;
+import com.android.internal.util.slim.ImageHelper;
+import com.android.internal.util.slim.DeviceUtils;
+import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
