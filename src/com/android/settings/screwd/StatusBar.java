43c6b5e2c15455127ef951265923ad00aad1773f
eyosen <abittin@gmail.com>
2015-07-26 21:07:09 +0300

    Settings: statusbar weather temperature placement (1/2)
    Left or right options
    
    Change-Id: I26a5f129c17bf041a6e84dbb21cb3acedb1a4f0b
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/screwd/StatusBar.java
+++ src/com/android/settings/screwd/StatusBar.java
@@ -64,11 +64,13 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 	
 	private static final String SHOW_CARRIER_LABEL = "status_bar_show_carrier";
     private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
-	private static final String STATUS_BAR_TEMPERATURE_STYLE = "status_bar_temperature_style";
+    private static final String STATUS_BAR_TEMPERATURE = "status_bar_temperature";
+    private static final String STATUS_BAR_TEMPERATURE_STYLE = "status_bar_temperature_style";
 
     private ListPreference mShowCarrierLabel;
     private SwitchPreference mCustomHeader;
 	private ListPreference mStatusBarTemperature;
+	private ListPreference mStatusBarTemperatureStyle;
 	
 
     @Override
@@ -109,14 +111,26 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         mCustomHeader.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0) == 1));
         mCustomHeader.setOnPreferenceChangeListener(this);
-		
-		// Status bar temp
-        mStatusBarTemperature = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE_STYLE);
-        int temperatureStyle = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0);
-        mStatusBarTemperature.setValue(String.valueOf(temperatureStyle));
+
+
+        // Statusbar temp
+        mStatusBarTemperature = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE);
+        int temperatureShow = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        mStatusBarTemperature.setValue(String.valueOf(temperatureShow));
         mStatusBarTemperature.setSummary(mStatusBarTemperature.getEntry());
         mStatusBarTemperature.setOnPreferenceChangeListener(this);
+
+        mStatusBarTemperatureStyle = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE_STYLE);
+        int temperatureStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mStatusBarTemperatureStyle.setValue(String.valueOf(temperatureStyle));
+        mStatusBarTemperatureStyle.setSummary(mStatusBarTemperatureStyle.getEntry());
+        mStatusBarTemperatureStyle.setOnPreferenceChangeListener(this);
+
+        enableStatusBarTemperatureDependents();
 
     }
 
@@ -134,14 +148,25 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
            Settings.System.putInt(resolver,
                    Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, value ? 1 : 0);
             return true;
-		} else if (preference == mStatusBarTemperature) {
-            int temperatureStyle = Integer.valueOf((String) newValue);
+        } else if (preference == mStatusBarTemperature) {
+            int temperatureShow = Integer.valueOf((String) newValue);
             int index = mStatusBarTemperature.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureStyle);
+            Settings.System.putIntForUser(
+                    resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureShow,
+                    UserHandle.USER_CURRENT);
             mStatusBarTemperature.setSummary(
                     mStatusBarTemperature.getEntries()[index]);
-            return true;	
+            enableStatusBarTemperatureDependents();
+            return true;
+        } else if (preference == mStatusBarTemperatureStyle) {
+            int temperatureStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarTemperatureStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(
+                    resolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, temperatureStyle,
+                    UserHandle.USER_CURRENT);
+            mStatusBarTemperatureStyle.setSummary(
+                    mStatusBarTemperatureStyle.getEntries()[index]);
+            return true;
         }
         return false;
     }
@@ -183,6 +208,17 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 	@Override
     protected int getMetricsCategory() {
         return MetricsLogger.APPLICATION;
+    }
+	
+	private void enableStatusBarTemperatureDependents() {
+        int temperatureShow = Settings.System.getIntForUser(getActivity()
+                .getContentResolver(), Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        if (temperatureShow == 0) {
+            mStatusBarTemperatureStyle.setEnabled(false);
+        } else {
+            mStatusBarTemperatureStyle.setEnabled(true);
+        }
     }
 
 }
