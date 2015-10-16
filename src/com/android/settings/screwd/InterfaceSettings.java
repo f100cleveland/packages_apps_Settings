11d0826350118aa9af108cd806497f211197259b
Corey Edwards <ensabahnur16@gmail.com>
2015-10-16 09:31:44 -0500

    Bring back Screw'd Interface settings
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ src/com/android/settings/screwd/InterfaceSettings.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2013 SlimRoms
+ * Copyright (C) 2014 Screw'd Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.screwd;
+
+import com.android.settings.notification.DropDownPreference;
+import com.android.settings.notification.DropDownPreference.Callback;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.preference.ListPreference;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.provider.SearchIndexableResource;
+
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InterfaceSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable{
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.screwd_interface_settings);
+		
+		PreferenceScreen prefSet = getPreferenceScreen();
+    }
+	
+	
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+	
+	
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+		final String key = preference.getKey();
+				
+        return false;
+    }
+	
+	public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.screwd_interface_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+    };
+	
+	@Override
+	protected int getMetricsCategory() {
+        return MetricsLogger.DISPLAY;
+    }
+	
+
+}
