82c91af5e5b35803d3f113aa569787f2995bd2e1
Lars Greiss <kufikugel@googlemail.com>
2016-01-12 21:29:42 +0000

    QS: Navbar Tile
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/screwd/qs/QSTileHolder.java
+++ src/com/android/settings/screwd/qs/QSTileHolder.java
@@ -146,6 +146,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_screenshot_tile;
                 break;
+			case QSConstants.TILE_NAVBAR:
+                resourceName = "ic_qs_navbar_on";
+                stringId = R.string.qs_navbar_tile;
+                break;	
             default:
                 return null;
         }
