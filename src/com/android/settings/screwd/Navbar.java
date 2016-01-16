f4263d0706978dc4c014b4b6475ced8af6fc8e2d
Corey Edwards <ensabahnur16@gmail.com>
2016-01-15 20:17:46 -0600

    Revert "Navigation bar button color [2/2]"
    This reverts commit 7718d8cb4896b6abb547c256b05922e6a0e08145.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	res/values/screwd_strings.xml

--- src/com/android/settings/screwd/Navbar.java
+++ src/com/android/settings/screwd/Navbar.java
@@ -34,7 +34,6 @@ import android.provider.Settings;
 import android.preference.ListPreference;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -53,8 +52,6 @@ import java.util.List;
 
 import com.android.internal.logging.MetricsLogger;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
 public class Navbar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 	
@@ -68,7 +65,6 @@ public class Navbar extends SettingsPreferenceFragment implements
     private static final String DIM_NAV_BUTTONS_TOUCH_ANYWHERE = "dim_nav_buttons_touch_anywhere";
 
     private SwitchPreference mKillAppLongPressBack;
-	private ColorPickerPreference mNavbarButtonTint;
     private SwitchPreference mDimNavButtons;
     private SlimSeekBarPreference mDimNavButtonsTimeout;
     private SlimSeekBarPreference mDimNavButtonsAlpha;
@@ -93,15 +89,6 @@ public class Navbar extends SettingsPreferenceFragment implements
         int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
                 KILL_APP_LONGPRESS_BACK, 0);
         mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
-		
-		// Navigation bar button color
-        mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVIGATION_BAR_TINT);
-        mNavbarButtonTint.setOnPreferenceChangeListener(this);
-        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.NAVIGATION_BAR_TINT, 0xffffffff);
-        String hexColor = String.format("#%08x", (0xffffffff & intColor));
-        mNavbarButtonTint.setSummary(hexColor);
-        mNavbarButtonTint.setNewPreviewColor(intColor);
 
         // SlimDim
         mDimNavButtons = (SwitchPreference) findPreference(DIM_NAV_BUTTONS);
@@ -224,14 +211,6 @@ public class Navbar extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                 Settings.System.DIM_NAV_BUTTONS_ANIMATE_DURATION,
                 Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mNavbarButtonTint) {
-            String hex = ColorPickerPreference.convertToARGB(
-                    Integer.valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.NAVIGATION_BAR_TINT, intHex);
             return true;
         }
         return false;
